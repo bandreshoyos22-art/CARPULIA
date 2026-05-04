8d32a3cda48e0fcfbd276c404b9e0c990b25dfef
martinorti <143116827+martinorti@users.noreply.github.com>
2026-05-04 15:26:13 -0500

    ruta para unirse a la lista de espera

--- backend/viajes/urls.py
+++ backend/viajes/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import ViajeViewSet
+from .views import ViajeViewSet, unirse_lista_espera
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'viajes', ViajeViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('viajes/<int:viaje_id>/lista-espera/', unirse_lista_espera, name='lista-espera'),
+]
